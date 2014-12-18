bfb6d93ada4140f165d4df67c8a181056549659f
Allan Legemaate <alegemaate@gmail.com>
2014-12-17 19:42:44 -0500

    Nicer GUI
    Made Piano graphic relevant and larger, cut out super high and super low
    notes.

--- main.cpp
+++ main.cpp
@@ -15,19 +15,29 @@ SAMPLE* piano;
 // Variables
 int piano_width = 0;
 int piano_height = 0;
+int piano_number = 0;
+int start_key = 0;
+int piano_y_offset = 0;
+
 double frequency = 0;
 
 // Program setup
 void setup(){
-  // Create buffer
-  buffer = create_bitmap( 640, 480);
-
   // Load instruments
   piano = load_sample( "piano.wav");
 
   // Width of piano tiles in px
-  piano_width = 60;
-  piano_height = piano_width * 3;
+  start_key = 50;
+  piano_width = 20;
+  piano_height = piano_width * 5;
+  piano_number = 62;
+  piano_y_offset = 10;
+
+  // Set up screen
+  set_gfx_mode( GFX_AUTODETECT_WINDOWED, piano_width * piano_number, piano_height + piano_y_offset, 0, 0);
+
+  // Create buffer
+  buffer = create_bitmap( SCREEN_W, SCREEN_H);
 }
 
 // Main game loop
@@ -36,13 +46,13 @@ void game(){
   rectfill( buffer, 0, 0, SCREEN_W, SCREEN_H, makecol(0,0,0));
 
   // Draw piano
-  for( int i = 0; i < 10; i ++){
-    rectfill( buffer, i* piano_width, 10, i* piano_width + piano_width, 10 + piano_height, makecol(0,0,0));
-    rectfill( buffer, i* piano_width + 1, 10, i* piano_width + piano_width - 1, 10 + piano_height, makecol(255,255,255));
+  for( int i = 0; i < piano_number; i ++){
+    rectfill( buffer, i* piano_width, piano_y_offset, i* piano_width + piano_width, piano_y_offset + piano_height, makecol(0,0,0));
+    rectfill( buffer, i* piano_width + 1, piano_y_offset, i* piano_width + piano_width - 1, piano_y_offset + piano_height, makecol(255,255,255));
   }
 
   // Change frequency depending on mouse location
-  frequency = 440 * pow(2.0, ((mouse_x/4) - 69.0)/12.0);
+  frequency = 440 * pow(2.0, ((mouse_x/piano_width + start_key) - 69.0)/12.0);
 
   // Play note
   if( mouse_b & 1){
@@ -50,7 +60,7 @@ void game(){
   }
 
   // Draw frequency to screen
-  textprintf_ex( buffer, font, 0, 0, makecol(0,0,0), makecol(255,255,255), "Frequency: %f Mouse_x: %i", frequency, (mouse_x/4));
+  textprintf_ex( buffer, font, 0, 0, makecol(0,0,0), makecol(255,255,255), "Frequency: %f Mouse_x: %i", frequency, (mouse_x/piano_width + start_key));
 
   // Cursor
   rectfill( buffer, mouse_x, mouse_y, mouse_x + 4, mouse_y + 4, makecol(255,0,0));
@@ -69,9 +79,8 @@ int main(){
   install_keyboard();
   set_palette(desktop_palette);
   set_color_depth(32);
-  set_gfx_mode( GFX_AUTODETECT_WINDOWED, 640, 480, 0, 0);
   install_sound(DIGI_AUTODETECT,MIDI_AUTODETECT,".");
-  set_window_title("Math Blocks");
+  set_window_title("Sample Piano!");
 
   // Setup game
   setup();
